698d426ad7866420329d99b433df2da7d5250123
JulianCienfuegos <melvyniandrag@gmail.com>
2015-04-27 00:22:36 -0400

    error norm written

--- Project/FEM_main_2d.m
+++ Project/FEM_main_2d.m
@@ -10,15 +10,15 @@ set(0,'DefaultAxesFontSize', 18)
 
 L2 = [];
 Linf = []; 
-
-for num = ['3']
+% % 
+for num = [ '1', '2', '3']
     f = strcat('2D_Problems/Problem_2.1/Dirichlet/Mesh', num ,'/*');
     disp(f)
     copyfile(f, '.')
-
+    probid = 'Problem 2.1 DBC ';    
+    
     read_2D_mesh();
     read_2D_input();
-
     K = zeros(nnodes);
     F = zeros(nnodes,1);
 
@@ -46,7 +46,7 @@ for num = ['3']
     saveas(plt, name)
     
     % Compute and plot exact solution -------------------------------------
-    U = exact_2D(XNODES,YNODES,'Problem 2.1 DBC ');
+    U = exact_2D(XNODES,YNODES,probid);
     figure()
     trimesh(CONN, XNODES, YNODES, U)
     plt2 = trimesh(CONN, XNODES, YNODES, u);
@@ -54,5 +54,36 @@ for num = ['3']
     title(ttl);
     name = strcat(ttl, '.jpg');
     saveas(plt2, name);
-
+    
+    % Compute error norms -------------------------------------------------
+    L2 = 0;
+    Linf = 0;
+    
+    for j = 1:nelems
+        nodelist = CONN(j,:);
+        xi = XNODES(nodelist);
+        yi = YNODES(nodelist);
+        ui = u(nodelist);
+       
+        A_e = 0.5*(xi(1)*yi(2) - yi(1)*xi(2) + xi(2)*yi(3) - yi(2)*xi(3) + ...
+            xi(3)*yi(1) - yi(3)*xi(1));
+        w = [1/6, 1/6, 1/6];
+        p1 = @(ksi, eta) 1 - ksi - eta;
+        p2 = @(ksi, eta) ksi;
+        p3 = @(ksi, eta) eta;
+        psi = [p1(0.5, 0), p1(0.5, 0.5), p1(0, 0.5);...
+               p2(0.5, 0), p2(0.5, 0.5), p2(0, 0.5);...
+               p3(0.5, 0), p3(0.5, 0.5), p3(0, 0.5)];
+        for k = [1:3]
+            u_h = dot(ui, psi(:,k));
+            x = dot(xi, psi(:,k));
+            y = dot(yi, psi(:,k));
+            
+            U = exact_2D(x, y, probid);
+            
+            L2 = L2 + w(k) * 2*A_e*(U - u_h)^2;
+            Linf = max(Linf, abs(U - u_h));
+        end
+    end
+    L2 = sqrt(L2);
 end
